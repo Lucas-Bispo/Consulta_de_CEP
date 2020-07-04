faa4579d9fe44599852488851bf4d7ea3474e185
Lucas-Bispo <lukaobispo@gmail.com>
2020-07-04 19:36:49 -0300

    projeto quase pronto, arrumar erro em http

--- web/main.dart
+++ web/main.dart
@@ -1,5 +1,20 @@
 import 'dart:html';
-import '';
+import 'dart:convert';
+import 'package:http/http.dart' as hattp;
 
 void main() {
+
+  querySelector('#search').onClick.listen((a) async{
+    String cep = (querySelector("#cep") as InputElement).value;
+    String url = 'http://viacep.com.br/ws/$cep/json/';
+
+    var response = await http.get(url);
+    var body = json.decode(response.body);
+    print(body);
+
+  (querySelector("#state") as InputElement).value = body['uf'];
+  (querySelector("#city") as InputElement).value = body['localidade'];
+  (querySelector("#neighborhood") as InputElement).value = body['bairro'];
+  (querySelector("#street") as InputElement).value = body['logradouro'];
+  });
 }
